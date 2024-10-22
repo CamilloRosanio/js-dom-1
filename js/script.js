0757500125b63c91ebceab3168a57d4d932b0f14
MyUsername <rosanio.c@gmail.com>
2024-10-22 18:12:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+console.log('JS CONNECTION OK')
+
+/*
+Consegna
+Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
+Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+BONUS
+Facciamo accendi e spegni:
+Al primo click la lampadina si accende e nel bottone compare la scritta "Spegni"
+Al secondo click la lampadina si spegne e nel bottone compare la scritta "Accendi"
+E così via...
+Per fare questo bonus potremmo aver bisogno di del metodo string.includes() */
+
+
+
+
+
+
+// HTML**********************************
+
+// costruisco l'HTML
+
+// uso un solo tag IMG e ci metto la lampadina spenta
+
+// creo un bottone
+
+
+// JS****************************************
+
+// Dichiaro prima di tutto delle costanti dove prendo le entità dall'HTML tramite ID/Classe/Altro
+
+// estraggo le proprietà che mi servono e verifico con console.log
+
+// in questo caso mi serve il src="" del tag immagine
+
+// in base al click del bottone, cambio il src dell'immagine switchando tra l'uno e l'altro
+
+// al contempo, in base al SRC dell immagine cambio la scritta del bottone
