ce428e5c864efb33aa93d6a2378206a36013c8d9
MyUsername <rosanio.c@gmail.com>
2024-10-23 09:33:35 +0200

    CORREZIONE 1 - Lampadina

--- js/script.js
+++ js/script.js
@@ -19,22 +19,19 @@ const myLamp = document.getElementById('myLamp');
 
 // Definisco la FUNZIONE che dovrà svolgere il mio bottone. In questo caso mi serve cambiare il src="" del tag img.
 // Al contempo, se voglio cambiare la scritta al bottone devo modificare il suo Inner Text insieme al src="" dell'img.
+// Aggiungo un EventListener al bottone, che nello specifico ascolti i Click (esistono categorie predefinite di eventi, non è un valore testuale casuale).
+// Definisco la funzione che dovrà svolgere il bottone una volta ascoltato il Click.
+
 myButton.addEventListener('click', () => {
     const lampSrc = myLamp.getAttribute('src');
     console.log(lampSrc);
 
     if (lampSrc.includes('./img/white_lamp.png')) {
         myLamp.setAttribute('src', './img/yellow_lamp.png');
-        myButton.innerText('Spegni');
+
     } else if (lampSrc.includes('./img/yellow_lamp.png')) {
         myLamp.setAttribute('src', './img/white_lamp.png')
-        myButton.innerText('Accendi');
+
     }
     }
 )
-
-// Aggiungo un EventListener al bottone, che nello specifico ascolti i Click (esistono categorie predefinite di eventi, non è un valore testuale casuale).
-// Definisco la funzione che dovrà svolgere il bottone una volta ascoltato il Click.
-
-
-// al contempo, in base al SRC dell immagine cambio la scritta del bottone
